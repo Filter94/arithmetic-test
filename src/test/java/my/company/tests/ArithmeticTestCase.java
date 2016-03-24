e6a3e8629d5606db747ed01c4376031c70d9728c
Filter <Filter>
2016-03-24 18:24:32 +0600

    Now it's single class again. Js engine removed.

--- src/test/java/my/company/tests/ArithmeticTestCase.java
+++ src/test/java/my/company/tests/ArithmeticTestCase.java
@@ -1,8 +1,7 @@
 package my.company.tests;
 
-import ru.yandex.qatools.allure.annotations.Parameter;
-import ru.yandex.qatools.allure.annotations.Description;
-import ru.yandex.qatools.allure.annotations.Attachment;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.*;
 
 import org.junit.Assert;
 import static org.junit.Assert.fail;
@@ -16,28 +15,26 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-
 /**
- * Base class for arithmetic tests.
+ * Class for arithmetic tests.
  * Implements tests parametrisation, execution and also attachment saving logic.
  */
 @RunWith(Parameterized.class)
 @Description("This is an example test suite")
-public class ArithmeticTestBase {
+public class ArithmeticTestCase {
     // Epsilon for float point comparison
     private static final double EPSILON = 1e-10;
     // Name of file with fixtures. Must be set by child class.
-    static String FIXTURES_NAME = "";
+    final private static String FIXTURES_NAME = "fixture.txt";
 
+    @Parameterized.Parameters()
     /**
      * Function for tests parametrisation
      * @return Iterable with constructor parameters
      * @throws Exception
      */
     public static Iterable<Object[]> data() throws Exception{
-        ArithmeticTestBase.saveTxtAttachment();
+        ArithmeticTestCase.saveTxtAttachment();
         URL dir_url = ClassLoader.getSystemResource(FIXTURES_NAME);
         List<String> fixturesArray = Files.readAllLines(Paths.get(dir_url.toURI()));
         List<Object[]> parametersArray = new ArrayList<>(0);
@@ -55,10 +52,10 @@ public class ArithmeticTestBase {
      * @param result raw string with expected result of test
      * @throws Exception
      */
-    ArithmeticTestBase(String operand1,
-                       String operand2,
-                       String operation,
-                       String result) throws Exception{
+    public ArithmeticTestCase(String operand1,
+                              String operand2,
+                              String operation,
+                              String result) throws Exception{
         this.operand1 = Double.parseDouble(operand1);
         this.operand2 = Double.parseDouble(operand2);
         this.operation = operation;
@@ -77,21 +74,43 @@ public class ArithmeticTestBase {
     @Parameter("Expected result")
     private double result;
 
+    @Title("Operation test")
+    @Features("Arithmetic operations support")
+    @Stories({"Support for arithmetic operations"})
+    @Test
     /**
      * Performs test. Saves text attachment if something goes wrong.
      * Gets result by evaluating expression by js engine.
      * @throws Exception
      */
     public void performTest() throws Exception{
+        Double result;
         try {
-            String expression = Double.toString(operand1) + operation + Double.toString(operand2);
-            ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("js");
-            Double result = (Double) engine.eval(expression);
-            Assert.assertEquals(this.result, result, ArithmeticTestBase.EPSILON);
+            switch(operation){
+                case "+":{
+                    result = operand1 + operand2;
+                    break;
+                }
+                case "-":{
+                    result = operand1 - operand2;
+                    break;
+                }
+                case "/":{
+                    result = operand1 / operand2;
+                    break;
+                }
+                case "*":{
+                    result = operand1 * operand2;
+                    break;
+                }
+                default:{
+                    throw new Exception("Unknown operation");
+                }
+            }
+            Assert.assertEquals(this.result, result, ArithmeticTestCase.EPSILON);
         }
         catch (Throwable e){
-            ArithmeticTestBase.saveTxtAttachment();
+            ArithmeticTestCase.saveTxtAttachment();
             throw e;
         }
     }
